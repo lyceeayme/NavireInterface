edb5db81177a3ed073a14b9e5e180b3ad3c11198
Ayme PIGNON <ayme.pignon.sio2023@lyc-bonaparte.fr>
2022-03-14 10:25:31 +0100

    Creation de la classe Passager

--- ConsoleApp1/ClassesMetier/Passager.cs
+++ ConsoleApp1/ClassesMetier/Passager.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace NavireHeritage.ClassesMetier
 {
-    class Passager
+    public class Passager
     {
+        private readonly string numPasseport;
+        private readonly string nom;
+        private readonly string prenom;
+        private readonly string nationalite;
+
+        public Passager(string numPasseport, string nom, string prenom, string nationalite)
+        {
+            this.numPasseport = numPasseport;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.nationalite = nationalite;
+        }
+
+        /// <summary>
+        /// Gets le numero de passeport.
+        /// </summary>
+        public string NumPasseport => this.numPasseport;
+
+        /// <summary>
+        /// Gets le nom du passager.
+        /// </summary>
+        public string Nom => this.nom;
+
+        /// <summary>
+        /// Gets le prenom du passager.
+        /// </summary>
+        public string Prenom => this.prenom;
+
+        /// <summary>
+        /// Gets la nationalite du passager.
+        /// </summary>
+        public string Nationalite => this.nationalite;
     }
 }
